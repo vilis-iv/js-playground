2737aea8b4e050d7d02c9368b19fefc7ed3c79ea
vilis-iv <sarcastrope@gmail.com>
2024-11-15 10:27:47 +0700

    method and path implementation

--- node_basic/server.js
+++ node_basic/server.js
@@ -1,16 +1,64 @@
 const http = require("http");
 
 const requestListener = (req, res) => {
-  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Powered-By", "Node.js");
 
-  res.statusCode = 200;
-  res.end("SERVER STARTED");
+  const { method, url } = req;
+
+  if (url === "/") {
+    if (method === "GET") {
+      res.statusCode = 200;
+      res.end("THIS IS ROOT");
+    } else {
+      res.statusCode = 400;
+      res.end(
+        JSON.stringify({
+          message: "INVALID REQUEST",
+        })
+      );
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      let body = [];
+
+      req.on("data", (chunk) => {
+        body.push(chunk);
+      });
+
+      req.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+
+        res.statusCode = 200;
+        res.end(
+          JSON.stringify({
+            message: `Hello ${name}`,
+          })
+        );
+      });
+    } else {
+      res.statusCode = 400;
+      res.end(
+        JSON.stringify({
+          message: "INVALID REQUEST",
+        })
+      );
+    }
+  } else {
+    res.statusCode = 404;
+    res.end(
+      JSON.stringify({
+        message: "INVALID URL!",
+      })
+    );
+  }
 };
 
 const server = http.createServer(requestListener);
 
 const port = 5000;
-const host = "127.0.1.1";
+const host = "localhost";
 
 server.listen(port, host, () => {
   console.log(`Server started 'http://${host}:${port}`);
